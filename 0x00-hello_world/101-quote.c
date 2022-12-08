be3c4a232fe0fca447adddb49e0785b97890fbd2
Weke Victor <35596661+kira-vik@users.noreply.github.com>
2022-12-08 13:13:31 +0300

    Fixed issue on 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote to the standard error.
  * Return: Always 1 (success)
  */
 int main(void)
 {
-     char *error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-     fprintf(stderr, error);
-     return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
