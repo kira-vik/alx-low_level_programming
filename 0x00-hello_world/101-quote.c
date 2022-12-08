84a0bd19ff70948a1ee27be39bc432c31ed58258
Weke Victor <35596661+kira-vik@users.noreply.github.com>
2022-12-08 13:04:29 +0300

    Fixed issue on 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+     char error [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, error);
 	return (1);
 }
