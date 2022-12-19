704919c4c945749ba99a9fa3e08e29e7af454272
Weke Victor <35596661+kira-vik@users.noreply.github.com>
2022-12-19 21:13:59 +0300

    Added 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int count;
 
-	while (s[i++])
-		len++;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
-
+	while (count--)
+	{
+		_putchar(*(s + (count)));
+	}
 	_putchar('\n');
 }
+
