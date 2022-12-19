d5610b6543b1cae93166b4f266ab76ca959f596c
Weke Victor <35596661+kira-vik@users.noreply.github.com>
2022-12-19 21:28:50 +0300

    Added 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (*src != '\0')
 	{
-		dest[index] = src[index];
+		dest[index] = *src;
 		index++;
+		src++;
 	}
-
 	return (dest);
 }
